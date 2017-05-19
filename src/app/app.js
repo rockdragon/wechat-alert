08411e32fa5a12c05143dd7bc1f418c8bab018ee
Locke <moyerock@gmail.com>
2017-05-19 10:33:31 +0800

    chore: initial commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,9 @@
-
+const { Wechaty } = require('wechaty')
 
 export function start() {
-
+  Wechaty.instance() // Singleton
+    .on('scan', (url, code) => console.log(`Scan QR Code to login: ${code}\n${url}`))
+    .on('login',       user => console.log(`User ${user} logined`))
+    .on('message',  message => console.log(`Message: ${message}`))
+    .init()
 }
